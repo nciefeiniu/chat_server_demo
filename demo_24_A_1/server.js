64931fad3a78907e6cfefc45444295ecb1c0a6ac
nicefeiniu <872938058@qq.com>
2024-11-01 16:30:06 +0800

    feat: 基本完成
    还差我得收藏页面

--- demo_24_A_1/server.js
+++ demo_24_A_1/server.js
@@ -1,6 +1,9 @@
 import { Client } from "https://deno.land/x/postgres@v0.19.3/mod.ts";
 import { serve } from "https://deno.land/std@0.90.0/http/server.ts";
-import { getCookies } from "https://deno.land/std@0.90.0/http/mod.ts"; 
+import {
+  getCookies,
+  setCookie,
+} from "https://deno.land/std@0.90.0/http/mod.ts";
 import * as bcrypt from "https://deno.land/x/bcrypt/mod.ts";
 
 const databaseInfo = {
@@ -14,7 +17,7 @@ const databaseInfo = {
 const serverPort = 8080;
 
 // 会话存储
-const sessions = {};
+var sessions = {};
 
 // 连接postgresql数据库
 const client = new Client(databaseInfo);
@@ -22,18 +25,19 @@ const client = new Client(databaseInfo);
 const decoder = new TextDecoder();
 const headers = new Headers();
 
-headers.set("Access-Control-Allow-Origin", "*");
-headers.set("Access-Control-Allow-Methods", "POST, OPTIONS");
+headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, HEAD, PUT");
 headers.set("Access-Control-Allow-Headers", "Content-Type");
+headers.set("content-type", "application/json");
+headers.set("Access-Control-Allow-Credentials", true);
 
-
-function generateRandomString(length=16) {
-  let result = '';
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+function generateRandomString(length = 16) {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   const charactersLength = characters.length;
 
   for (let i = 0; i < length; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
 
   return result;
@@ -116,18 +120,18 @@ async function login(req) {
   if (isPasswordCorrect) {
     // 登录成功
     const sessionId = generateRandomString();
-    sessions[sessionId] = { username: userInfo[1], isLoggedIn: true, id: userInfo[0] }
-    const loginHeaders = new Headers();
-    loginHeaders.set("Access-Control-Allow-Origin", "*");
-    loginHeaders.set("Access-Control-Allow-Methods", "POST, OPTIONS");
-    loginHeaders.set("Access-Control-Allow-Headers", "Content-Type");
-    loginHeaders.set("Set-Cookie", `sessionId=${sessionId}; Path=/; HttpOnly`);
+    sessions[sessionId] = {
+      username: userInfo[1],
+      isLoggedIn: true,
+      id: userInfo[0],
+    };
+    // console.log('login success, ', sessions)
     return req.respond({
-      headers: loginHeaders,
+      headers: headers,
       status: 200,
       body: JSON.stringify({
         message: "Login successful!",
-        data: { username: username, id: userInfo[0] },
+        data: { username: username, id: userInfo[0], token: sessionId },
       }),
     });
   }
@@ -139,6 +143,125 @@ async function login(req) {
   });
 }
 
+async function createNewLink(req) {
+  // 创建新的链接
+  const { title, desc, link } = await req.json(req);
+  const currentUserID = req.currentUserID;
+  await client.queryArray(
+    `INSERT INTO links (link, title, "desc", user_id) VALUES ('${link}', '${title}', '${desc}', ${currentUserID});`
+  );
+  await client.queryArray("COMMIT");
+  return req.respond({
+    headers,
+    status: 201,
+    body: JSON.stringify({
+      message: "Link created successfully!",
+      data: { title, desc, link },
+    }),
+  });
+}
+
+async function getLinks(req) {
+  // 获取所有链接
+  const currentUserID = req.currentUserID;
+  let result = [];
+  if (currentUserID) {
+    result = await client.queryObject(
+      `SELECT a.id,link,title,"desc",score, u.username FROM links a join users u on u.id = a.user_id order by score desc;`
+    );
+  } else {
+    result = await client.queryObject(
+      `SELECT a.id,link,title,"desc",score, u.username FROM links a join users u on u.id = a.user_id order by score desc;`
+    );
+  }
+
+  return req.respond({
+    headers,
+    status: 200,
+    body: JSON.stringify({
+      message: "Links fetched successfully!",
+      data: result.rows,
+    }),
+  });
+}
+
+async function updateLinkScore(req, linkID) {
+  // 评分，分数是 1-5分， 其中 1，2 代表 差评，3，4，5 代表好评
+  /*
+  规则：
+      如果评分是1，那就是扣分的，扣2分
+      评分是2，扣1分
+      评分是3，不扣不加
+      评分是4，加1分
+      评分是5，加2分
+
+
+  而且，如果评分是 4，5，还需要把这个link加入到收藏表中，也就是我们的收藏夹中
+  */
+  const { score } = await req.json(req);
+
+  const currentUserID = req.currentUserID;
+  await client.queryArray("BEGIN");
+
+  const result = await client.queryArray(`
+    SELECT * FROM score_detail WHERE user_id = ${currentUserID} AND link_id = ${linkID}
+    `);
+
+  if (result.rows.length > 0) {
+    // 如果评价过了，那就只能更新数据，不能新建！！！
+    await client.queryArray(`
+        UPDATE score_detail SET score = ${score} WHERE user_id = ${currentUserID} AND link_id = ${linkID}
+        `);
+  } else {
+    await client.queryArray(`
+        INSERT INTO score_detail (user_id, link_id, score) VALUES (${currentUserID}, ${linkID}, ${score})
+        `);
+  }
+  // 计算总分
+  const commentData = await client.queryObject(`
+    SELECT score FROM score_detail WHERE link_id = ${linkID}
+    `);
+  let totalScore = 0;
+  for (let i = 0; i < commentData.rows.length; i++) {
+    const currentScore = commentData.rows[i].score;
+    if (currentScore === 1) {
+      totalScore -= 2;
+    } else if (currentScore === 2) {
+      totalScore -= 1;
+    } else if (currentScore === 3) {
+      totalScore += 0;
+    } else if (currentScore === 4) {
+      totalScore += 1;
+    } else if (currentScore === 5) {
+      totalScore += 2;
+    }
+  }
+
+  await client.queryArray(
+    `UPDATE links SET score = ${totalScore} WHERE id = ${linkID};`
+  );
+
+  if (score > 3) {
+    await client.queryArray(
+      `INSERT INTO good_link (user_id, link_id) VALUES (${currentUserID}, ${linkID});`
+    );
+  } else {
+    // 如果分数低于 3 分，就从收藏夹中移除
+    await client.queryArray(
+      `DELETE FROM good_link WHERE user_id = ${currentUserID} AND link_id = ${linkID};`
+    );
+  }
+  await client.queryArray("COMMIT");
+  req.respond({
+    headers,
+    status: 201,
+    body: JSON.stringify({
+      message: "Score successfully!",
+      data: { id: linkID, score: score },
+    }),
+  });
+}
+
 async function main() {
   await client.connect(); // 连接数据库
   const server = serve({ port: serverPort });
@@ -147,8 +270,21 @@ async function main() {
     const pathname = req.url;
     const method = req.method;
     const now = new Date();
+    headers.set("Access-Control-Allow-Origin", req.headers.get("origin"));
+
     console.log(`(${now.toISOString()}) Request: ${method} ${pathname}`);
     req.json = readJson;
+    // console.log(req)
+    const requestCookies = getCookies(req);
+    const currentUserSession = requestCookies.sessionID;
+    console.log("currentUserSession: ", currentUserSession);
+    if (sessions[currentUserSession]) {
+      // 已登录的用户
+      req.currentUserID = sessions[currentUserSession].id;
+    }
+    // console.log(sessions)
+    console.log("current user id: ", req.currentUserID);
+
     try {
       if (method === "OPTIONS") {
         // 预检请求，直接返回 204 No Content
@@ -159,8 +295,23 @@ async function main() {
       } else if (pathname === "/login" && method === "POST") {
         // 登录
         await login(req);
-      } else if (pathname === "/update" && method === "PUT") {
-      } else if (pathname === "/delete" && method === "DELETE") {
+      } else if (pathname === "/links" && req.currentUserID) {
+        // 提交带有标题和描述的新链接
+        if (method === "POST") {
+          await createNewLink(req);
+        } else if (method === "GET") {
+          await getLinks(req);
+        } else {
+          req.respond({ headers, status: 405 });
+        }
+      } else if (
+        pathname.startsWith("/link/score") &&
+        method === "POST" &&
+        req.currentUserID
+      ) {
+        // 用户给这个链接评分
+        const linkID = pathname.split("/")[3];
+        await updateLinkScore(req, linkID);
       } else {
         req.respond({ headers, status: 404 });
       }
